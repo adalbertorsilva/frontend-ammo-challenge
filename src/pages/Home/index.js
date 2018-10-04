176d5ee06f59422c8e7f685e06c86ee019ba27e8
adalbertorsilva <adalbertorsilvajr@gmail.com>
2018-10-04 08:08:47 -0300

    - project config

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import logo from '../../logo.svg'
+import './page.css'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+        <input placeholder='OK OK OK OK OK'/>
+      </div>
+    );
+  }
+}
+
+export default Home
